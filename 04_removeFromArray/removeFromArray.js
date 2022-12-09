85e076b5663b77c1406daec60e73a64e6419ebe3
zocc-n <znanic@yahoo.com>
2022-12-09 22:14:39 +0100

    Test first 4 functions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...theArgs) {
+    theArgs = theArgs.sort();
+    for(let i=0; i<arr.length; i++){
+        for(elem of theArgs){
+            if(elem === arr[i]){
+                arr.splice(i,1);
+            }
+        }
+    }
+    return arr;
 };
-
+//console.log(removeFromArray([1,2,3,4], 3, 2));
 // Do not edit below this line
 module.exports = removeFromArray;
