4c54308aee4a293b56f50981a686c3a89ada9bd1
zocc-n <znanic@yahoo.com>
2022-12-09 22:38:21 +0100

    Declared min and max valued

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,6 +2,16 @@ const sumAll = function(a,b) {
     if((typeof a != 'number' || typeof b != 'number') || (a<0 || b<0)){
         return 'ERROR';
     }
+    
+    if(a<b){
+        let min = a;
+        let max = b;
+    } else{
+        let min = b;
+        let max = a;
+    }
+
+    
 };
 
 // Do not edit below this line
