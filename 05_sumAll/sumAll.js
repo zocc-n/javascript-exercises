f7e0062e837d09f4e92b6993ce4087e4f9b18d1f
zocc-n <znanic@yahoo.com>
2022-12-09 22:32:42 +0100

    Test passed where expected value is ERROR

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if((typeof a != 'number' || typeof b != 'number') || (a<0 || b<0)){
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
