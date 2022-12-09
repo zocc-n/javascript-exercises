786e889db9a72e25c73c1c9261b92f3d21fac85b
zocc-n <znanic@yahoo.com>
2022-12-09 22:51:16 +0100

    Passed all tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,16 +2,22 @@ const sumAll = function(a,b) {
     if((typeof a != 'number' || typeof b != 'number') || (a<0 || b<0)){
         return 'ERROR';
     }
-    
+
+    let min, max;
     if(a<b){
-        let min = a;
-        let max = b;
+        min = a;
+        max = b;
     } else{
-        let min = b;
-        let max = a;
+        min = b;
+        max = a;
     }
 
+    let sum = 0;
+    for(let i=min; i<=max; i++){
+        sum += i;
+    }
     
+    return sum;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.spec.js
+++ 05_sumAll/sumAll.spec.js
@@ -1,13 +1,13 @@
 const sumAll = require('./sumAll')
 
 describe('sumAll', () => {
-  test.skip('sums numbers within the range', () => {
+  test('sums numbers within the range', () => {
     expect(sumAll(1, 4)).toEqual(10);
   });
-  test.skip('works with large numbers', () => {
+  test('works with large numbers', () => {
     expect(sumAll(1, 4000)).toEqual(8002000);
   });
-  test.skip('works with larger number first', () => {
+  test('works with larger number first', () => {
     expect(sumAll(123, 1)).toEqual(7626);
   });
   test('returns ERROR with negative numbers', () => {
