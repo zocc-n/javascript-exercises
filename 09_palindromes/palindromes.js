7490494364ec8511fa7d27b1e21e49f7c552a48f
zocc-n <znanic@yahoo.com>
2022-12-10 00:39:26 +0100

    Passed all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let processedStr = str.toLowerCase().replace(/[^a-z]/g, "");
+    return processedStr.split('').reverse().join('') === processedStr;
 };
 
 // Do not edit below this line
