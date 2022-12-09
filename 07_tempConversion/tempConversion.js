4d6e11d33119aff209b0e0206e5ae6e39e097302
zocc-n <znanic@yahoo.com>
2022-12-09 23:58:32 +0100

    Passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius = (temp - 32) * (5 / 9);
+  return Math.round(celsius *10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = temp * 9/5 + 32;
+  return Math.round(fahrenheit * 10)/10;
 };
 
 // Do not edit below this line
