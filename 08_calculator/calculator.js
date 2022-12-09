46eb3f0eef5cbc66387723d7612911aabe8578dd
zocc-n <znanic@yahoo.com>
2022-12-10 00:16:59 +0100

    Passed all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, current) => total * current);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num === 0) return 1;
+  let fact = 1; 
+  for(let i=num; i>0; i--){
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
