f08fe47ba2e859e348abc08a8c1bd5be99525a0f
zocc-n <znanic@yahoo.com>
2022-12-10 00:53:45 +0100

    Passed all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 1) return 'OOPS';
+    let fib = [1, 1];
+    for(let i=2; i<num; i++){
+        fib.push(fib[fib.length-1] + fib[fib.length-2]);
+    }
+    return fib[fib.length-1];
 };
 
 // Do not edit below this line
